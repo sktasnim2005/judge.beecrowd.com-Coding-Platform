623e04cf9c325bed05353a0e41592ca3d3abbc7f
Sk Tasnim Ur Rahman <162748135+sktasnim2005@users.noreply.github.com>
2024-10-12 03:21:33 +0600

    Create 1005 - Average 1.cc

---
+++ 1005 - Average 1.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double A, B;
+    cin >> A >> B;
+    
+    double X = (A * 3.5 + B * 7.5) / 11.0;
+    
+    
+    cout << fixed << setprecision(5) << "MEDIA = " << X << endl;
+    
+    return 0;
+}
